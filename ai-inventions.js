91e1012e41a0291f8e92a5262e62f63a81c1fe17
benitoRepository <122936158+benitoRepository@users.noreply.github.com>
2023-10-05 16:16:00 -0700

    Add files via upload

--- ai-inventions.js
+++ ai-inventions.js
@@ -184,13 +184,13 @@ function random_choic(array){
 }
 
 window.onload=function donenow(){
-	document.querySelector('#categories_hbutton').addEventListener('click', () => {
+	/*document.querySelector('#categories_hbutton').addEventListener('click', () => {
 		document.querySelector('.menu').classList.toggle('open');
 		document.querySelector('.menu_close').classList.toggle('show');
 	});
 	document.querySelector('.menu_close').addEventListener('click', () =>{
 		document.querySelector('.menu').classList.toggle('open');
 		document.querySelector('.menu_close').classList.toggle('show');
-	});
+	});*/
 	createButtonsWithListener(categories);
 }
